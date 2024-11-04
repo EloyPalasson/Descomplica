cc9f88cf3d8d7e38b9f08c83e1d4b6163d769539
MatheusEloyPalasson <retheus.palasson@gmail.com>
2024-11-04 09:32:08 -0300

    Adicionando mais alguns exercicios de exception

--- Módulo-1A/Desenvolvimento Dinâmico/Exception/exception.js
+++ Módulo-1A/Desenvolvimento Dinâmico/Exception/exception.js
@@ -7,10 +7,11 @@ function dividir(a,b) {
 }
 
 try {
-    const result = dividir(10,2);
-    console.log("O resultado é",result);
+    const primeiroResultado = dividir(10,2);
+    console.log("O resultado é",primeiroResultado);
+    const segundoResultado = dividir(5,0);
 } catch (error) {
-    console.log("Ocorreu uma exception", e.message);
+    console.log("Ocorreu uma exception:", error.message);
 }
 
 /////////////////////////////////
@@ -27,7 +28,73 @@ try {
     const segundaSenha = "M293u8"
     verificarTamanhoSenha(segundaSenha);
 } catch (error) {
-    console.log("Ocorreu uma exception", error.message);
+    console.log("Ocorreu uma exception:", error.message);
 }
 
 /////////////////////////////////////////////////
+
+const numeros = [1,2,3,4,5];
+//const letras = ["M", "J", "K"];
+
+try {
+    const dobrarNumeros = numeros.map((num) => {
+        if (typeof num !== "number") {
+            throw new Error ("O array só aceita números")
+        }
+        return num * 2;
+    })
+    console.log("Array com número dobrados:", dobrarNumeros);
+} catch (error) {
+    console.log("Ocorreu uma exception:", error.message);
+}
+
+/////////////////////////////////////////////////
+
+function processarNome(nome,callback) {
+    if (typeof nome !== "string") {
+        callback(new Error("O nome deve ser uma string"))
+        return;
+    }
+
+    if (nome.length === 0) {
+        callback(new Error("O nome não pode estar vazio"));
+        return;
+    }
+
+    callback(null,"Nome processado com sucesso");
+}
+
+processarNome("Matheus",(error,result) => {
+    if (error) {
+        console.log("Ocorreu um problema: " + error.message);
+    } else {
+        console.log("Status -", result);
+    }
+})
+
+///////////////////////////////////////////////
+
+function verificarArrayPositivo(numeros) {
+    if (!Array.isArray(numeros)) {
+        throw new Error("O argumento deve ser de um array");
+    }   
+
+    const positivos = numeros.every((num) => num > 0);
+    if (!positivos) {
+        throw new Error("O array deve conter apenas números positivos");
+    }
+    return true;
+}
+
+try {
+    const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+    const isPositive = verificarArrayPositivo(numbers);
+
+    console.log("O array contém números positivos -", isPositive);
+
+} catch (error) {
+    console.log("Ocorreu um exception:", error.message);
+}
+
+
